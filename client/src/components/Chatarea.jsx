04c9a4a8e5d934928d95fed303a0cb708b5d4b4c
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-08-09 23:00:52 +0530

    header

--- client/src/components/Chatarea.jsx
+++ client/src/components/Chatarea.jsx
@@ -1,10 +1,12 @@
-import { Stack } from '@mui/material';
+import { Avatar, Stack } from '@mui/material';
 import React, { useState } from 'react';
 import bgImg from '../assets/bgImg.png';
 import LockIcon from "@mui/icons-material/Lock";
+import { deepOrange, deepPurple } from "@mui/material/colors";
 
 const Chatarea = () => {
-  const [activeUserData, setActiveUserData] = useState([]);
+  const [activeUserData, setActiveUserData] = useState([1]);
+  const name = "Chitresh";
   return (
     <div style={{ background: "#DAE5F5", width: "100vw", height: "100vh" }}>
       {activeUserData.length == 0 ? (
@@ -12,7 +14,7 @@ const Chatarea = () => {
           <Stack
             justifyContent={"center"}
             alignItems={"center"}
-            sx={{ height: "100vh"}}
+            sx={{ height: "100vh" }}
           >
             <div style={{ textAlign: "center" }}>
               <img src={bgImg} alt="bgImg" />
@@ -33,14 +35,41 @@ const Chatarea = () => {
                 Use Pocket Notes on up to 4 linked devices and 1 mobile phone
               </p>
             </div>
-            <Stack direction={"row"} justifyContent={"center"} alignItems={"center"}>
+            <Stack
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
               <LockIcon />
               <p>end-to-end encrypted</p>
             </Stack>
           </Stack>
         </>
       ) : (
-        <>chat</>
+        <>
+          <header style={{marginBottom:"1rem", position:"static"}}>
+            <Stack
+              direction={"row"}
+              justifyContent={"start"}
+              alignItems={"center"}
+              gap={2}
+              px={2}
+              sx={{ "&:hover": { cursor: "pointer" }, background: "#001F8B" }}
+            >
+              <Avatar sx={{ bgcolor: deepOrange[500] }}>
+                {name.charAt(0).toUpperCase()}
+              </Avatar>
+              <p
+                className=""
+                style={{ fontSize: "1.2rem", fontWeight: "bold", color: "#ffffff" }}
+              >
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </p>
+            </Stack>
+            </header>
+            
+            
+        </>
       )}
     </div>
   );
