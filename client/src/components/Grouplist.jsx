0c686d7537bd8fe840d8d0cfec8cbc19a33317f9
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-08-09 21:36:04 +0530

    side menu

--- client/src/components/Grouplist.jsx
+++ client/src/components/Grouplist.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Avatar from "@mui/material/Avatar";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+import { Stack } from '@mui/material';
 
-const Grouplist = () => {
+const Grouplist = ({ name }) => {
+    
+    const handleList = () => {
+        console.log("clicked");
+    }
+
   return (
-    <div className='px-1'>
-      
-    </div>
-  )
+    <Stack
+          direction={"row"}
+          justifyContent={"start"}
+          alignItems={"center"}
+          gap={2}
+          px={2}
+          onClick={handleList}
+          sx={{"&:hover":{cursor:"pointer"}}}
+    >
+      <Avatar sx={{ bgcolor: deepOrange[500] }}>
+              {name.charAt(0).toUpperCase()}
+          </Avatar>
+      <p className="" style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </p>
+    </Stack>
+  );
 }
 
 export default Grouplist
--- client/src/components/Sidemenu.jsx
+++ client/src/components/Sidemenu.jsx
@@ -1,16 +1,43 @@
-import React, { useState } from 'react'
-import Grouplist from './Grouplist';
+import React, { useEffect, useState } from "react";
+import Grouplist from "./Grouplist";
+import axios from 'axios';
+import './sidemenu.css';
 
 const Sidemenu = () => {
   const [list, setList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/group/fetchAllGroup")
+      .then((res) => { 
+        console.log(res?.data?.allGroups);
+        setList(res?.data?.allGroups);
+      })
+      .catch((error) => { 
+        console.log(error);
+      })
+  }, []);
 
   return (
-    <div style={{width: "15rem", padding:"2rem 2rem", textAlign:"center"}}>
-      <h2>Pocket Notes</h2>
-      <Grouplist/>
-      
+    <div
+      style={{
+        width: "15rem",
+        padding: "1rem 1rem 1rem 2rem",
+        textAlign: "center",
+        overflowY: "hidden",
+      }}
+    >
+      <h2 style={{ marginTop: "1.5rem" }}>Pocket Notes</h2>
+      <div
+        style={{
+          height: `${86}vh`,
+          overflowY: "scroll",
+        }}
+      >
+        {list.map((item, index) => (
+          <Grouplist key={index} name={item?.groupName} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sidemenu;
