28c7c07cd425cad0a8bc9a216d67274029a7f23a
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-08-10 10:31:57 +0530

    push

--- server/app.js
+++ server/app.js
@@ -21,6 +21,8 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/", (req, res) => {
+    res.send("server is running");
+})
 app.use("/group", groupRoutes);
 app.use("/notes", notesRoutes);
