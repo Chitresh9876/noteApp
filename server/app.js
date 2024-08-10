1bb345ba17f8f93ced98ec40201566fdbef73277
Chitresh Kumar Gupta <104379067+Chitresh9876@users.noreply.github.com>
2024-08-10 10:20:36 +0530

    vercel

--- server/app.js
+++ server/app.js
@@ -11,7 +11,13 @@ config({ path: "./config/config.env" });
 export const app = express();
 
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
